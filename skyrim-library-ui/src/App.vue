989d5cb9c3b28fa4a706d587571e2ddb242bca7e
Роман Жданов <romanizh@ulh.ru>
2023-07-16 14:27:32 +0300

    Added The Header component to layout.

--- skyrim-library-ui/src/App.vue
+++ skyrim-library-ui/src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
+import TheHeader from './components/layout/TheHeader.vue'
 </script>
 
 <template>
+  <TheHeader />
   <div class="container">
     <RouterView />
   </div>
