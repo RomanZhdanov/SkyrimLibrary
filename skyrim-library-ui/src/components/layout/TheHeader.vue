ef9768add63175fa2f1476c9099e42bd6976686a
Roman Zhdanov <zhdanov.roma@gmail.com>
2025-02-08 17:26:47 +0400

    Added Home link to header menu.

--- skyrim-library-ui/src/components/layout/TheHeader.vue
+++ skyrim-library-ui/src/components/layout/TheHeader.vue
@@ -16,6 +16,9 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
             <router-link class="nav-link" to="/books">Books</router-link>
           </li>
           <li class="nav-item">
